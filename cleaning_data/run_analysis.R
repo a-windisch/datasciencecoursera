90e19bbe938d807b6e2aa429901bda2e727a4e0e
a-windisch <andreas.windisch@yahoo.com>
2018-05-24 16:04:03 -0500

    added dynamically generated codebook

--- cleaning_data/run_analysis.R
+++ cleaning_data/run_analysis.R
@@ -18,7 +18,7 @@ cat("---------------------------------------------\n")
 ##################################################
 
 cat("Setting wdir (change as required), loading data...")
-setwd("YOUR WDIR HERE, SEE README.md")
+setwd("/home/andreas/learning/Data_Science_Johns_Hopkins_University/03_Getting_and_Cleaning_Data/course_project")
 Xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE,sep="")
 Ytrain <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE,sep="")
 Xtest <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE,sep="")
@@ -96,7 +96,7 @@ cat("Reading activity names from activity_labels.txt.\n")
 activitynames <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,sep="")
 activitynames$V1 <- as.character(activitynames$V1)
 activitynames$V2 <- as.character(activitynames$V2)
-cat("Loading the plyr package (need mapvalues()).")
+cat("Loading the plyr package (need mapvalues()).\n")
 library(plyr)
 cat("Replacing activity values with activity names.\n")
 Xtotal$activity <- as.character(Xtotal$activity)
@@ -160,6 +160,106 @@ colnames(Xtotal) <- gsub("mean","Mean",colnames(Xtotal))
 colnames(Xtotalaverage) <- gsub("mean","Mean",colnames(Xtotalaverage))
 colnames(Xtotal) <- gsub("std","Std",colnames(Xtotal))
 colnames(Xtotalaverage) <- gsub("std","Std",colnames(Xtotalaverage))
+
+##################################################
+#              creating code book                #
+##################################################
+cat("---------------------------------------------\n")
+cat("Creating code book 'CodeBook.md'.\n")
+sink("CodeBook.md")
+cat("#  coursera Getting and Cleaning Data\n")
+cat("### code book for course project\n")
+cat("### Andreas Windisch, andreas.windisch@yahoo.com ")
+
+cat("\n\n")
+cat("Based on this [1] data set:\n")
+cat("==================================================================\n")
+cat("Human Activity Recognition Using Smartphones Dataset\n")
+cat("Version 1.0\n")
+cat("==================================================================\n")
+cat("Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto.\n")
+cat("Smartlab - Non Linear Complex Systems Laboratory\n")
+cat("DITEN - Università degli Studi di Genova.\n")
+cat("Via Opera Pia 11A, I-16145, Genoa, Italy.\n")
+cat("activityrecognition@smartlab.ws\n")
+cat("www.smartlab.ws\n")
+cat("==================================================================\n")
+cat("\n\n")
+cat("## 1. Data sets \n")
+cat("There are two data sets, Xtotal and Xtotalaverage.\n")
+cat("- Dimensions Xtotal       : ", dim(Xtotal),"\n")
+cat("- Dimensions Xtotalaverage: ", dim(Xtotalaverage),"\n")
+
+cat("\n\n")
+
+cat("## 2. Variables - general description \n")
+cat("The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz. \n")
+cat("Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). \n")
+cat("Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). \n")
+cat("These signals were used to estimate variables of the feature vector for each pattern:  \n")
+cat("'XYZ' is used to denote 3-axial signals in the X, Y and Z directions.\n")
+cat("tBodyAccXYZ\n")
+cat("tGravityAccXYZ\n")
+cat("tBodyAccJerkXYZ\n")
+cat("tBodyGyroXYZ\n")
+cat("tBodyGyroJerkXYZ\n")
+cat("tBodyAccMag\n")
+cat("tGravityAccMag\n")
+cat("tBodyAccJerkMag\n")
+cat("tBodyGyroMag\n")
+cat("tBodyGyroJerkMag\n")
+cat("fBodyAccXYZ\n")
+cat("fBodyAccJerkXYZ\n")
+cat("fBodyGyroXYZ\n")
+cat("fBodyAccMag\n")
+cat("fBodyAccJerkMag\n")
+cat("fBodyGyroMag\n")
+cat("fBodyGyroJerkMag\n")
+cat("\n")
+cat("The set of variables that were estimated from these signals are: \n")
+cat("Mean: Mean value\n")
+cat("Std: Standard deviation\n")
+
+
+cat("## 3. Variables in 'Xtotal' \n")
+ct <- 1L
+
+for( i in colnames(Xtotal)){
+   cat("-Variable: ", i, "\n")
+   cat("--Column number: ", ct, "\n")
+   if( i != "activity"){
+      cat("--Range: [-1,1]\n")
+      cat("--Type : numeric\n\n")
+   } else {
+      cat(unique(Xtotal$activity),"\n")
+      cat("--Type : character\n\n") 
+   }
+   ct <- ct + 1
+}
+
+cat("\n\n")
+cat("## 4. Variables in 'Xtotalaverage' \n")
+cat("\n\n")
+ct <- 1L
+cat("The variables in this data set are averaged for every activity.\n\n")
+for( i in colnames(Xtotal)){
+   cat("-Variable: ", i, "\n")
+   cat("--Column number: ", ct, "\n")
+   if( i != "activity"){
+      cat("--Range: [-1,1]\n")
+      cat("--Type : numeric\n\n")
+   } else {
+      cat(unique(Xtotalaverage$activity),"\n")
+      cat("--Type : character\n\n") 
+   }
+   ct <- ct + 1
+}
+cat("\n\n")
+cat("[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz.\n")
+cat("Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine.\n") 
+cat("International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012\n")
+sink()
+cat("Code book 'CodeBook.md' created.\n")
 
 ##################################################
 #               concluding remarks               #
